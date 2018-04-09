9efd93b5b955a63d541e6ed628b975e43ee0faec
Dmitry Galperin <dgalperin@gmail.com>
2018-04-09 17:37:11 -0400

    Update to proper document title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 import { OWNER, MOVIES, TITLE, GITHUB } from './constants'
 import Movie from './components/Movie'
 
 class App extends Component {
+
+  componentDidMount() {
+    document.title = TITLE
+  }
 
   render() {
     return (
@@ -13,17 +16,13 @@ class App extends Component {
         {MOVIES.map((movie, index) => <Movie key={index} movie={movie} />)}
         <div className="footer text-center mb-3">
           <a href={GITHUB} className="btn btn-lg btn-light">
-  			    <i class="fa fa-lg fa-github" aria-hidden="true"></i>
+  			    <i className="fa fa-lg fa-github mr-1" aria-hidden="true"></i>
             <span>{`${OWNER}'s Github`}</span>
     		  </a>
         </div>
       </div>
     )
   }
-}
-
-App.propTypes = {
-  title: PropTypes.string.isRequired
 }
 
 export default App
